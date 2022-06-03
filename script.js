98240a413a7f661ef77084cd56e2177a6a93ca79
HenriqueDMenezes <henriquebm16@hotmail.com>
2022-06-02 22:18:57 -0300

    Logica para verificar se o computador esta conectado a internet

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const connectionStatus = async () => {
+  try{
+    const fetchResult = await fetch("https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Jean_Froissart%2C_Chroniques%2C_154v%2C_12148_btv1b8438605hf336%2C_crop.jpg/300px-Jean_Froissart%2C_Chroniques%2C_154v%2C_12148_btv1b8438605hf336%2C_crop.jpg")
+    return fetchResult.status >= 200 &&  fetchResult.status < 300;
+  }catch (err){
+    return false;   
+  }
+};
+
+setInterval(async () => {
+  const result = await connectionStatus();
+  const statusDisplay = document.getElementById("status");
+  statusDisplay.textContent = result ? "Online" : "Offline"
+},3000)
+
